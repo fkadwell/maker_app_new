366de4ed78511e87bd4f82c6a4d1a79cdb825baa
Fnobly <tommy@blytheclan.com>
2024-05-23 18:52:14 -0700

    thomas

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // his widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
+        // his is the theme of your application.
         //
-        // TRY THIS: Try running your application with "flutter run". You'll see
+        // TRY HIS: Try running your application with "flutter run". You'll see
         // the application has a purple toolbar. Then, without quitting the app,
         // try changing the seedColor in the colorScheme below to Colors.green
         // and then invoke "hot reload" (save your changes or press the "hot
