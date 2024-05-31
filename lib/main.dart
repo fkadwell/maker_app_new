dc0915ac5cbe1459c6aa3c404e33e0558e3df20b
Frank Kadwell <fkadwell@d3informationservices.com>
2024-05-30 21:41:38 -0700

    Thomas Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,167 +1,93 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  //stateless widget that represents the entire app.
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Page',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'Roboto', // Set the default font to Roboto
+        //     colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF004D71)),
+        scaffoldBackgroundColor: const Color(0xFF004D71),
+        useMaterial3: true,
       ),
-      home: LoginPage(),
+      home: const MyHomePage(title: 'Create an Account'),
     );
   }
 }
 
-class LoginPage extends StatelessWidget {
-  //stateless widget representing the Login page
-  //stateless widget representing the login page
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //appBar: AppBar(
+      //  backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      //  title: Text(widget.title),
+      //),
       body: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Expanded(
-            flex: 1,
-            child: Image.asset(
-              'assets/Login/design.png', // Change to your image path
-              fit: BoxFit.cover,
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Center(
-                    child: Text(
-                      'Login',
-                      style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF014C72), // Text color
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10.0),
-                  Center(
-                    child: Text(
-                      'Sign in to continue',
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        color: Color(0xFF014C72), // Text color
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 20.0),
-                  Text(
-                    'Please enter Email:',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: Color(0xFF014C72), // Text color
-                    ),
-                  ),
-                  SizedBox(height: 5.0),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Email',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  SizedBox(height: 10.0),
-                  Text(
-                    'Please enter Password:',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: Color(0xFF014C72), // Text color
-                    ),
-                  ),
-                  SizedBox(height: 5.0),
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  SizedBox(height: 20.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      TextButton(
-                        onPressed: () {
-                          // Action when "Create an account" is pressed
-                        },
-                        child: Text(
-                          'Create an account',
-                          style: TextStyle(
-                            color: Color(0xFF014C72), // Text color
-                          ),
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          // Action when "Reset Password" is pressed
-                        },
-                        child: Text(
-                          'Reset Password',
-                          style: TextStyle(
-                            color: Color(0xFF014C72), // Text color
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10.0),
-                  Align(
-                    alignment: Alignment.center,
-                    child: TextButton(
-                      onPressed: () {
-                        // Action when "Need Help?" is pressed
-                      },
-                      child: Text(
-                        'Need Help ?',
-                        style: TextStyle(
-                          color: Color(0xFF014C72), // Text color
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10.0),
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Action when "Login" is pressed
-                      },
-                      child: Text(
-                        'LOGIN',
-                        style: TextStyle(
-                          color: Colors.white, // Button text color
-                          fontSize: 20.0, // Button text size
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue, // Button background color
-                        minimumSize: Size(double.infinity, 50), // Button size
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+          Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset("assets/login/images/edmonds_logo.png", width: 650),
+                Image.asset("assets/login/images/makerspace_logo.png", width: 300)
+              ]),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(style: TextStyle(fontSize: 30, color: Colors.white), 'Create an Account'),
+              const SizedBox(height: 40),
+              const Text(
+                style: TextStyle(fontSize: 16, color: Colors.white),
+                'Please input a valid Email and Password to create an account.',
               ),
-            ),
+              const SizedBox(height: 20),
+              const SizedBox(
+                width: 250,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintStyle: TextStyle(color: Colors.grey),
+                    labelStyle: TextStyle(color: Colors.white),
+                    labelText: 'Email',
+                    hintText: 'Ex: example@example.com',
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 250,
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintStyle: TextStyle(color: Colors.grey),
+                    labelStyle: TextStyle(color: Colors.white),
+                    labelText: 'Password',
+                    hintText: 'Ex: hunter2',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF003a55)),
+                child: const Text(
+                    style: TextStyle(color: Colors.white), 'Create Account'),
+              ),
+            ],
           ),
         ],
       ),
